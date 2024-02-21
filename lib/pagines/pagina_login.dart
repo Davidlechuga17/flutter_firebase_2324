95f327f9a559913b176737edfe4404d11f2767dd
David <davidlechuga@ceroca.cat>
2024-02-21 20:23:30 +0100

    UI Login iniciada

---
+++ lib/pagines/pagina_login.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_2324/components/textfield_auth.dart';
+
+class PaginaLogin extends StatefulWidget {
+  const PaginaLogin({super.key});
+
+  @override
+  State<PaginaLogin> createState() => _PaginaLoginState();
+}
+
+class _PaginaLoginState extends State<PaginaLogin> {
+
+  final TextEditingController controllerEmail = TextEditingController();
+  final TextEditingController controllerPassword = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 250, 183, 159),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
+            const Icon(
+              Icons.fireplace,
+              size: 120,
+              color: Color.fromARGB(255, 255, 240, 218),
+            ),
+        
+            const Text(
+              "Benvingut/da de nou",
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 240, 218),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+        
+            const Row(
+              children: [
+                Expanded(
+                  child: Divider(
+                    thickness: 1,
+                    color: Color.fromARGB(255, 255, 240, 218),
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 4,),
+                  child: Text(
+                    "Fes login",
+                    style: TextStyle(color: Color.fromARGB(255, 255, 240, 218),),
+                  ),
+                ),
+
+                Expanded(
+                  child: Divider(
+                    thickness: 1,
+                    color: Color.fromARGB(255, 255, 240, 218),
+                  ),
+                ),
+              ],
+            ),
+              
+            TextFieldAuth(
+              controller: controllerEmail, 
+              obscureText: false, 
+              hintText: "Email",
+            ),
+
+            TextFieldAuth(
+              controller: controllerPassword, 
+              obscureText: true, 
+              hintText: "Password",
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}

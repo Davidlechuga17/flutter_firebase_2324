946964a0af1cb51ec6f075d999031b24494beafd
David <davidlechuga@ceroca.cat>
2024-04-12 18:50:57 +0200

    FilePicker funcionant i mostrant imatge

--- lib/pagines/editar_dades_usuari.dart
+++ lib/pagines/editar_dades_usuari.dart
@@ -66,7 +66,7 @@ class _EditarDadesUsuariState extends State<EditarDadesUsuari> {
               // Boto per obrir el FilePicker.
               //==============================
               GestureDetector(
-                onTap: _triaImatge(),
+                onTap: _triaImatge,
                 child: Container(
                   padding: const EdgeInsets.all(10),
                   decoration: BoxDecoration(
@@ -91,10 +91,19 @@ class _EditarDadesUsuariState extends State<EditarDadesUsuari> {
 
               //Visor del resultat del FilePicker.
               //==================================
+              Container(
+                child: _imatgeSeleccionadaWeb == null && _imatgeSeleccionadaApp == null ?
+                Container() : 
+                kIsWeb ? 
+                Image.memory(_imatgeSeleccionadaWeb!, fit: BoxFit.fill,) :
+                Image.file(_imatgeSeleccionadaApp!, fit: BoxFit.fill,),
+              ),
 
 
               //Visor del resultat de carregar la imatge de Firebase Storage.
               //=============================================================
+
+
 
             ],
           ),
